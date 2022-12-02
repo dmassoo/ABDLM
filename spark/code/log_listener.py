8492c13a7245a18def12348cd3b2eea0cd52d1e7
Dmitrii <dvvorotnikov@gmail.ru>
2022-12-02 22:19:13 +0300

    log listener

--- spark/code/log_listener.py
+++ spark/code/log_listener.py
@@ -1,16 +1,40 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from cassandra.cluster import Cluster
 import time
+import cassandra.amdlm_cassandra_configs as ccfg
 
 # Wait kafka and spark to start
 time.sleep(15)
+
+# Attach to cassandra
+cluster = Cluster(['my-cassandra'], port=9042)
+session = cluster.connect()
+
+# Initialize keyspace abd table
+session.execute(f"create ${ccfg.keyspace} IF NOT EXISTS metrics with replication = "
+                "{'class' : 'SimpleStrategy', 'replication_factor':1}")
+session.execute(f"use ${ccfg.keyspace}")
+
+session.execute("""
+CREATE TABLE IF NOT EXISTS logs (
+ timestamp timestamp,
+ level text,
+ microservice_id text,
+ operation_type text,
+ action_id text
+ user_id text,
+ value int,
+ PRIMARY KEY(action_id)
+);""")
 
 scala_version = '2.12'
 spark_version = '3.3.1'
 packages = [
     f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
-    'org.apache.kafka:kafka-clients:3.3.1'
+    'org.apache.kafka:kafka-clients:3.3.1',
+    f'com.datastax.spark:spark-cassandra-connector_{scala_version}:{spark_version}'
 ]
 
 spark = SparkSession.builder\
@@ -36,20 +60,22 @@ kafkaDF = spark \
             .selectExpr("CAST(value AS STRING)") 
 
 
-schema = StructType([ \
-    StructField("timestamp", TimestampType(), True), \
-    StructField("level", StringType(), True), \
-    StructField("microservice_id", StringType(), True), \
-    StructField("operation_type", StringType(), True), \
-    StructField("action_id", StringType(), True), \
-    StructField("user_id", StringType(), True) \
-  ])
-# TODO: may be add checkpoint
+schema = StructType([
+    StructField("timestamp", TimestampType(), True),
+    StructField("level", StringType(), True),
+    StructField("microservice_id", StringType(), True),
+    StructField("operation_type", StringType(), True),
+    StructField("action_id", StringType(), True),
+    StructField("user_id", StringType(), True)
+])
+
 query = kafkaDF.select(from_json(col("value"), schema).alias("t")) \
-            .select("t.timestamp", "t.level", "t.microservice_id", "t.operation_type", "t.action_id", "t.user_id") \
-            .writeStream.format("console") \
-            .option("truncate", "false") \
-            .outputMode("append") \
-            .trigger(processingTime='5 seconds') \
-            .start() \
+            .select("t.timestamp", "t.level", "t.microservice_id", "t.operation_type", "t.action_id", "t.user_id")\
+            .writeStream\
+            .option("checkpointLocation", '/code/checkpoints/')\
+            .format("org.apache.spark.sql.cassandra")\
+            .option("keyspace", "metrics")\
+            .option("table", "data_table")\
+            .trigger(processingTime='30 seconds') \
+            .start()\
             .awaitTermination()
