caa8a5fec10546e6a273ec8a70ad6bc6095897ae
Dmitrii <dvvorotnikov@gmail.ru>
2022-12-04 20:20:08 +0300

    added transactions processing to business metrics job

--- spark/code/abdlm_cassandra_configs.py
+++ spark/code/abdlm_cassandra_configs.py
@@ -6,6 +6,7 @@ cassandra_nodes = ['my-cassandra:9042']
 
 
 metricsSchema = StructType([
+    StructField("id", StringType(), False),
     StructField("timestamp", TimestampType(), True),
     StructField("microservice_id", StringType(), True),
     StructField("operation_type", StringType(), True),
--- spark/code/views_transactions_agg.py
+++ spark/code/views_transactions_agg.py
@@ -12,20 +12,28 @@ time.sleep(15)
 cluster = Cluster(['my-cassandra'], port=9042)
 session = cluster.connect()
 
-# Initialize keyspace abd table
+# Initialize keyspace and tables
 session.execute(f"create keyspace IF NOT EXISTS {ccfg.keyspace} with replication = "
                 "{'class' : 'SimpleStrategy', 'replication_factor':1}")
 session.execute(f"use {ccfg.keyspace}")
 
-topic = 'metrics'
-
 session.execute("""
 CREATE TABLE IF NOT EXISTS views (
+ id text,
  timestamp timestamp,
  views int,
- PRIMARY KEY(timestamp)
+ PRIMARY KEY(timestamp, id)
 );""")
 
+session.execute("""
+CREATE TABLE IF NOT EXISTS transactions (
+ id text,
+ timestamp timestamp,
+ transactions int,
+ PRIMARY KEY(timestamp, id)
+);""")
+
+# Spark Stream from Kafka setup
 scala_version = '2.12'
 spark_version = '3.3.1'
 packages = [
@@ -36,10 +44,12 @@ packages = [
 
 spark = SparkSession.builder \
     .master("spark://my-spark-master:7077") \
-    .appName("Views Application") \
+    .appName("Views and Transactions Application") \
     .config("spark.jars.packages", ",".join(packages)) \
     .config('spark.cassandra.connection.host', ','.join(ccfg.cassandra_nodes)) \
     .getOrCreate()
+
+topic = 'metrics'
 
 kafkaDF = spark \
     .readStream \
@@ -53,20 +63,18 @@ kafkaDF = spark \
 
 print('printing kafka df')
 kafkaDF.printSchema()
-table = 'views'
-
 
 # .withWatermark("timestamp", "15 minutes") \
-views = kafkaDF \
+metrics = kafkaDF \
     .select(from_json(col("value"), ccfg.metricsSchema).alias('t'))\
     .select("t.*")\
     .withWatermark("timestamp", "10 minutes")
 
 print('printing kafka df typed')
-views.printSchema()
+metrics.printSchema()
 
 
-v2 = views \
+views = metrics \
     .select("timestamp") \
     .filter(col("operation_type") == "VIEW") \
     .groupBy(
@@ -76,22 +84,40 @@ v2 = views \
     .select("window.start", "count") \
     .withColumnRenamed("start", "timestamp") \
     .withColumnRenamed("count", "views")
-v2.printSchema()
+views.printSchema()
 
-query = v2.writeStream\
-    .option("checkpointLocation", '/opt/') \
+transaction_events = ['VIEW', 'BUY', 'CANCEL', 'REFUND']
+transactions = metrics \
+    .select("timestamp") \
+    .filter(col("operation_type") in transaction_events) \
+    .groupBy(
+        window("timestamp", "15 minutes")
+    ) \
+    .count() \
+    .select("window.start", "count") \
+    .withColumnRenamed("start", "timestamp") \
+    .withColumnRenamed("count", "views")
+transactions.printSchema()
+
+# Writing pre-aggregates to Cassandra
+table_views = 'views'
+table_transactions = 'transactions'
+
+queryViews = views.writeStream \
+    .option("checkpointLocation", '/opt/views') \
     .format("org.apache.spark.sql.cassandra") \
     .option("keyspace", ccfg.keyspace) \
-    .option("table", table) \
+    .option("table", table_views) \
     .trigger(processingTime='5 seconds') \
-    .start() \
-    .awaitTermination()
+    .start()
 
-# query = v2.writeStream\
-#     .option("checkpointLocation", '/opt/') \
-#     .outputMode("update") \
-#     .format("console") \
-#     .option("truncate", False) \
-#     .trigger(processingTime='5 seconds') \
-#     .start() \
-#     .awaitTermination()
+queryTransactions = views.writeStream \
+    .option("checkpointLocation", '/opt/transactions') \
+    .format("org.apache.spark.sql.cassandra") \
+    .option("keyspace", ccfg.keyspace) \
+    .option("table", table_views) \
+    .trigger(processingTime='5 seconds') \
+    .start()
+
+queryViews.awaitTermination()
+queryTransactions.awaitTermination()
