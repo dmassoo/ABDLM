2bc6f3bfe4589ae15d1b2c5d0750113c5c7855c4
Dmitrii <dvvorotnikov@gmail.ru>
2022-12-04 19:12:28 +0300

    renamed logs app spark

--- spark/code/log_listener.py
+++ spark/code/log_listener.py
@@ -41,7 +41,7 @@ packages = [
 
 spark = SparkSession.builder \
     .master("spark://my-spark-master:7077") \
-    .appName("kafka-metric-spark") \
+    .appName("Logs App") \
     .config("spark.jars.packages", ",".join(packages)) \
     .config('spark.cassandra.connection.host', ','.join(ccfg.cassandra_nodes)) \
     .getOrCreate()
