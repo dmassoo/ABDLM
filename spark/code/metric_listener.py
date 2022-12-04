235d808175c22749d3098b974414d664c0f0acad
Dmitrii <dvvorotnikov@gmail.ru>
2022-12-05 01:01:58 +0300

    rename metrics job

--- spark/code/metric_listener.py
+++ spark/code/metric_listener.py
@@ -42,7 +42,7 @@ packages = [
 
 spark = SparkSession.builder\
     .master("spark://my-spark-master:7077")\
-    .appName("kafka-metric-spark")\
+    .appName("Metrics Application")\
     .config("spark.jars.packages", ",".join(packages))\
     .config('spark.cassandra.connection.host', ','.join(ccfg.cassandra_nodes))\
     .getOrCreate()
